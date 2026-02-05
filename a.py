4b30e53d13a11eb3a5671a723900198c6715eb21
Samanta Thapa <samantathapaa@gmail.com>
2026-02-05 12:52:20 +0545

    initial commit

---
+++ a.py
@@ -0,0 +1,11 @@
+from src.ad import add
+from src.su import sub
+from src.mu import mul
+num1=int(input("enter first number"))
+num1=int(input("enter second number"))
+
+add(num1,num2)
+sub(num1,num2)
+mul(num1,num2)
+
+print("from add, the sum is",sum)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import mymodule
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+
+sum_of_no=mymodule.add(num1,num2)
+subsofnum=mymodule.sub(num1,num2)
+print("Sum of the two numbers are:",sum_of_no)
+print("Difference of the two numbers are:",subsofnum)
